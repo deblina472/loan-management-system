0f115e3d149caa513465e76b9cce586dbd0c81a3
deblina472 <103497085+deblina472@users.noreply.github.com>
2024-04-01 21:47:37 +0530

    Create BaseInterestRates.java

---
+++ src/main/java/com/cts/entities/BaseInterestRates.java
@@ -0,0 +1,26 @@
+package com.cts.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Base_Interest_Rates")
+public class BaseInterestRates {
+    @Id
+    @Column(name = "Id")
+    private int id;
+    @Column(name = "Loan_Type")
+    private String loanType;
+    @Column(name = "Base_Interest_Rate")
+    private float baseInterestRate;
+
+
+}
