26061045acaf94c7341198627517ad84a66ee619
deblina472 <103497085+deblina472@users.noreply.github.com>
2024-04-06 10:13:24 +0530

    Update BaseInterestRatesServiceImpl.java

--- src/main/java/com/cts/services/BaseInterestRatesServiceImpl.java
+++ src/main/java/com/cts/services/BaseInterestRatesServiceImpl.java
@@ -1,9 +1,9 @@
-package com.cognizant.services;
+package com.cognizant.services.classes;
 
 import com.cognizant.dto.BaseInterestRatesDTO;
 import com.cognizant.entities.BaseInterestRates;
 import com.cognizant.repositories.BaseInterestRatesRepository;
-import lombok.Data;
+import com.cognizant.services.interfaces.BaseInterestRatesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +12,19 @@ import java.util.Iterator;
 import java.util.List;
 
 
+/**
+ * @author Deblina Das
+ * This is a service class where business logic for getting list of interest rates
+ */
 @Service
 public class BaseInterestRatesServiceImpl implements BaseInterestRatesService {
     @Autowired
     private BaseInterestRatesRepository baseInterestRatesRepository;
 
-
+    /**
+     * get list of interest rates
+     * @return
+     */
     @Override
     public List<BaseInterestRatesDTO> getInterestRateList() {
         List<BaseInterestRatesDTO> baseInterestRatesDTOList = new ArrayList<>();
