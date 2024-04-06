ac3cb1885336308fd349d89c963125b99fb7d1b8
deblina472 <103497085+deblina472@users.noreply.github.com>
2024-04-06 10:28:47 +0530

    Create BaseInterestRatesRepositoryTest.java

---
+++ src/test/java/com/cts/repositories/BaseInterestRatesRepositoryTest.java
@@ -0,0 +1,92 @@
+package com.cognizant.repository;
+
+import com.cognizant.LoanPlansModuleApplication;
+import com.cognizant.entities.BaseInterestRates;
+import com.cognizant.repositories.BaseInterestRatesRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@ContextConfiguration(classes = LoanPlansModuleApplication.class)
+public class BaseInterestRatesRepositoryTest {
+
+    @Autowired
+    private BaseInterestRatesRepository baseInterestRatesRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testFindAllPositive() {
+        BaseInterestRates baseInterestRates = new BaseInterestRates();
+        baseInterestRates.setId(10);
+        baseInterestRates.setLoanType("Home");
+        baseInterestRates.setBaseInterestRate(8.5f);
+        entityManager.persist(baseInterestRates);
+        Iterable<BaseInterestRates> it = baseInterestRatesRepository.findAll();
+        assertTrue(it.iterator().hasNext());
+
+    }
+
+    @Test
+    public void testFindAllNegative() {
+        baseInterestRatesRepository.deleteAll();
+        Iterable<BaseInterestRates> it = baseInterestRatesRepository.findAll();
+        assertTrue(!it.iterator().hasNext());
+
+    }
+
+
+    @Test
+    public void testFindByIdPositive() {
+
+        BaseInterestRates baseInterestRates = new BaseInterestRates();
+        baseInterestRates.setId(1);
+        baseInterestRates.setLoanType("Home");
+        baseInterestRates.setBaseInterestRate(8.5f);
+        entityManager.persist(baseInterestRates);
+        Optional<BaseInterestRates> bir = baseInterestRatesRepository.findById(1);
+        assertTrue(bir.isPresent());
+    }
+
+    @Test
+    public void testFindByIdNegative() {
+
+        Optional<BaseInterestRates> bir = baseInterestRatesRepository.findById(89);
+        assertTrue(!bir.isPresent());
+    }
+
+    @Test
+    public void testSavePositive() {
+
+        BaseInterestRates baseInterestRates = new BaseInterestRates();
+        baseInterestRates.setId(1);
+        baseInterestRates.setLoanType("Home");
+        baseInterestRates.setBaseInterestRate(8.5f);
+        baseInterestRatesRepository.save(baseInterestRates);
+        Optional<BaseInterestRates> bir = baseInterestRatesRepository.findById(1);
+        assertTrue(bir.isPresent());
+    }
+
+    @Test
+    public void testDeletePositive() {
+
+        BaseInterestRates baseInterestRates = new BaseInterestRates();
+        baseInterestRates.setId(1);
+        baseInterestRates.setLoanType("Home");
+        baseInterestRates.setBaseInterestRate(8.5f);
+        entityManager.persist(baseInterestRates);
+        baseInterestRatesRepository.delete(baseInterestRates);
+        Optional<BaseInterestRates> bir = baseInterestRatesRepository.findById(1);
+        assertTrue(!bir.isPresent());
+
+    }
+
+
+}
