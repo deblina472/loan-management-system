bc372fbd64685b0e45eda2260783f41d85785be3
deblina472 <103497085+deblina472@users.noreply.github.com>
2024-04-28 15:16:44 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO Base_Interest_Rates (Id, Loan_Type, Base_Interest_Rate)
+VALUES
+(1, 'Home', 8.5),
+(2, 'Personal', 10),
+(3, 'Medical', 7.5),
+(4, 'Vehicle', 8.0);
+
+insert into users values('Raj','manager','manager');
+insert into users values('Riya','customer','customer');
+
